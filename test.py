be3327a01b47eb4e0c5c6d4e85ea54c7adaaea95
Sawaransh <mittal.sawaransh@gmail.com>
2026-07-28 09:03:39 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,21 @@
+print("Sawaransh")
+
+a = 20;
+b = 10; 
+
+print(a+b)
+
+c = 'Sawaransh' 
+d = 'Harshit'
+e = c+d
+
+print(c, "+" ,d ,'-->', e);
+
+m = 4
+n = 3
+
+sum = m + n 
+
+p = "Sawaransh" 
+
+print(p + str(sum))
